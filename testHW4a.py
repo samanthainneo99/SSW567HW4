1d41f15c31b70bd5db1d8f20334aed73f8c372ed
unknown <samanthainneo@gmail.com>
2022-03-02 10:19:36 -0500

    testing for travis again

--- testHW4a.py
+++ testHW4a.py
@@ -15,14 +15,14 @@ class TestGithubAPI(unittest.TestCase):
         response = requests.get('https://api.github.com/users/samanthainneo99/repos') 
         # Confirm that the request-response cycle completed successfully.
         assert_true(response.ok)  
-    def test_request_response2(response):
+    def test_request_response2(response): #next example from the third link
         response == FindRepos('samanthainneo9999')
         assert_is_not_none(response)
 
-    # @patch('requests.get')
-    # def test1(self, mock_get):
-    #     mock_get.return_value.ok = False
-    #     self.assertEqual(FindRepos('samanthainneo9999'), "Cannot find requested user")
+    @patch('requests.get')
+    def test1(self, mock_get):
+        mock_get.return_value.ok = False
+        self.assertEqual(FindRepos('samanthainneo9999'), "Cannot find requested user")
     # @patch('requests.get')
     # def test2(self, mock_request):
     #     mock_request.get('https://api.github.com/users/samanthainneo99/repos')
