08bef83ef0650ab37f32c5f66ed32e0a9e548970
unknown <samanthainneo@gmail.com>
2022-03-01 22:27:51 -0500

    second commit with mocking

--- testHW4a.py
+++ testHW4a.py
@@ -14,11 +14,11 @@ class TestGithubAPI(unittest.TestCase):
     def test1(self, mock_get):
         mock_get.return_value.ok = True
         self.assertEqual(FindRepos('samanthainneo9999'), "Cannot find requested user")
-    def test2(self, mock_get):
-        mock_get.return_value.ok = True
+    def test2(self, mock_request):
+        mock_request.get('https://api.github.com/users/samanthainneo99/repos')
         self.assertEqual(FindRepos('samanthainneo99'), 11)
-    def test3(self, mock_get):
-        mock_get.return_value.ok = True
+    def test3(self, mock_request):
+        mock_request.get('https://api.github.com/users/samanthainneo99/repos')
         self.assertEqual(FindRepos('samanthainneo'), "No Repositories")
     def test_request_response(self):#This is the example given by the third link provided in the assignment
         # Send a request to the API server and store the response.
