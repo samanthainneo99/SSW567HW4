8c4ad7645c72ce0b3ead72e5c3c9d580acd0ddeb
unknown <samanthainneo@gmail.com>
2022-03-02 10:05:43 -0500

    add @patch above all tests

--- testHW4a.py
+++ testHW4a.py
@@ -14,12 +14,15 @@ class TestGithubAPI(unittest.TestCase):
     def test1(self, mock_get):
         mock_get.return_value.ok = False
         self.assertEqual(FindRepos('samanthainneo9999'), "Cannot find requested user")
+    @patch('requests.get')
     def test2(self, mock_request):
         mock_request.get('https://api.github.com/users/samanthainneo99/repos')
         self.assertEqual(FindRepos('samanthainneo99'), 11)
+    @patch('requests.get')
     def test3(self, mock_request):
         mock_request.get('https://api.github.com/users/samanthainneo99/repos')
         self.assertEqual(FindRepos('samanthainneo'), "No Repositories")
+    @patch('requests.get')
     def test_request_response(self):#This is the example given by the third link provided in the assignment
         # Send a request to the API server and store the response.
         response = requests.get('https://api.github.com/users/samanthainneo99/repos') 
